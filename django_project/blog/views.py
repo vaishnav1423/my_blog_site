dc248cb032223638500b3f475a632866d03e00a9
Vaishnav Kedar <vaishnav1423@gmail.com>
2022-05-27 12:45:10 +0530

    With Templates with CSS

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -20,7 +20,7 @@ def home(request):
     context={
         'posts':posts
     }  # To Pass variable as Context to Template
-    return render(request,'home.html',context)
+    return render(request,'blog/home.html',context)
 
 def about(request):
-    return render(request,'about.html',{'title': "About"})
+    return render(request,'blog/about.html',{'title': "About"})
