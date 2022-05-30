1a3c2620dd95220dc2be1593346acac90c38ef7b
Vaishnav Kedar <vaishnav1423@gmail.com>
2022-05-30 12:58:06 +0530

    Forms for user registration

--- django_project/users/views.py
+++ django_project/users/views.py
@@ -7,6 +7,7 @@ def register(request):
     if request.method=='POST':
         form=UserCreationForm(request.POST)     # request.POST will specifically happens when we click submit button on signup
         if form.is_valid():                     # is_valid will validate the form entries and return true if valid
+            form.save()                         #This will actually create the user 
             username=form.cleaned_data.get('username')
             messages.success(request,f'Account Created for user {username}!!')  #sends success message to next page
             return redirect('blog_db-home')     # redirect back to home page 
